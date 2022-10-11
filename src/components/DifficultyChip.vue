ed0933f87775e260c13aa648e7d6687ed3c8161e
dgarc1758 <Garciadarwin305@gmail.com>
2022-10-11 00:01:19 -0500

    Initial removal change

--- src/components/DifficultyChip.vue
+++ src/components/DifficultyChip.vue
@@ -17,6 +17,6 @@ const props = defineProps({
 
 <style lang="postcss" scoped>
 .difficulty {
-  @apply absolute bottom-6 left-8 rounded-full px-4 py-2 font-bold capitalize text-slate-700 text-white;
+  @apply absolute bottom-6 left-8 rounded-full px-4 py-2 font-bold capitalize text-slate-700;
 }
 </style>

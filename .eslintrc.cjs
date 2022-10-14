5342b6cc3592f4a49aa3c235f0a54e543a1aeb69
dgarc1758 <Garciadarwin305@gmail.com>
2022-10-14 11:31:54 -0500

    init clean up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,20 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution') Calculating...
 
 module.exports = {
   root: true,
   extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'prettier'],
   plugins: ['prettier'],
-  parserOptions: {
+  parserOptions:{
     ecmaVersion: 'latest',
   },
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 120,
-        tabWidth: 2,
-        useTabs: false,
-        semi: false,
-        singleQuote: true,
-        jsxSingleQuote: true,
-        trailingComma: 'all',
-        bracketSpacing: true,
-        bracketSameLine: false,
-        arrowParens: 'always',
-      },
-    ],
-    'arrow-body-style': 'off',
-    'prefer-arrow-callback': 'off',
+     'prettier/prettier': ['error'],
+     'arrow-body-style': 'off',
+     'prefer-arrow-callback': 'off',
   },
-  env: {
+  env:
+  {
     browser: true,
     node: true,
   },
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import MainLayout from "@/components/MainLayout.vue";
+
 </script>
 
 <template>
-  <MainLayout />
+  
+  <RouterView />
 </template>
